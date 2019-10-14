9a3f00fc1ce032d7a99dad3d87ac5dfd1af31a8b
tuVsha <55377453+tuVsha@users.noreply.github.com>
2019-10-14 11:48:33 +0500

    Add files via upload

---
+++ SentencesParserTask.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TextAnalysis
+{
+    static class SentencesParserTask
+    {
+        public static List<List<string>> ParseSentences(string text)
+        {
+            var sentencesList = new List<List<string>>();
+            var result = new List<List<string>>();
+            var sentence = new List<string>();
+            var word = "";
+            foreach (var symbol in text)
+            {
+                if (symbol == '.' || symbol == '!' || symbol == '?' || symbol == ';' || symbol == ':' || symbol == '(' || symbol == ')')
+                {
+                    //sentence.Add(word);
+                    sentence = new List<string>();
+                }
+                else if (symbol == ' ')
+                {
+                    sentence.Add(word);
+                    word = "";
+                }
+                else word += symbol;
+            }
+            return sentencesList;
+        }
+    }
+}
